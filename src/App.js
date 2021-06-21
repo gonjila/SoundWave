05929c72b93d4d60ac19ece6b7610acf78ae018f
gonjila <gonjilashvili.lasha@yahoo.com>
2021-06-21 06:42:36 +0400

    added files lazy loader

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
+import React, { Suspense } from 'react';
 import { Switch, Route } from 'react-router';
 import styled from 'styled-components';
 
 import Home from './COMPONENTS/Home';
-import Discover from './COMPONENTS/Discover';
-import Join from './COMPONENTS/Join';
+// import Discover from './COMPONENTS/Discover';
+// import Join from './COMPONENTS/Join';
+
+const Discover = React.lazy(() => import('./COMPONENTS/Discover'));
+const Join = React.lazy(() => import('./COMPONENTS/Join'));
 
 function App() {
     return (
         <Container>
-            <Switch>
-                <Route exact path="/">
-                    <Home />
-                </Route>
-                <Route exact path="/discover">
-                    <Discover />
-                </Route>
-                <Route exact path="/join">
-                    <Join />
-                </Route>
-            </Switch>
+            <Suspense fallback={<div>page is loading...</div>}>
+                <Switch>
+                    <Route exact path="/">
+                        <Home />
+                    </Route>
+                    <Route exact path="/discover">
+                        <Discover />
+                    </Route>
+                    <Route exact path="/join">
+                        <Join />
+                    </Route>
+                </Switch>
+            </Suspense>
         </Container>
     );
 }
